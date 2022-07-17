d05275ccc2b002271b70539546e2515fe3b41f76
rickie maweu <rickiemaweu@gmail.com>
2022-07-17 23:37:39 +0300

    a commit message

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,9 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
   );
 }
 
-export default Article;
+export default Article
